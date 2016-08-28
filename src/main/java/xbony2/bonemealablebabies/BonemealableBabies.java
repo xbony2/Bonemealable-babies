0b6a07122d487b120e6643a127903d7be7d751c1
Eric Schneider <xbony22@gmail.com>
2016-08-27 20:23:03 -0400

    .

--- src/main/java/xbony2/bonemealablebabies/BonemealableBabies.java
+++ src/main/java/xbony2/bonemealablebabies/BonemealableBabies.java
@@ -1,13 +1,15 @@
 package xbony2.bonemealablebabies;
 
 import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemDye;
+import net.minecraft.util.EnumActionResult;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.EntityInteractSpecific;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = BonemealableBabies.MODID, version = BonemealableBabies.VERSION)
@@ -24,9 +26,12 @@ public class BonemealableBabies {
 		@SubscribeEvent
 		public void boneMealAnimals(EntityInteractSpecific event){
 			if(event.getItemStack() != null && event.getItemStack().getItem() == Items.DYE && event.getItemStack().getItemDamage() == 15 && event.getTarget() != null && event.getTarget() instanceof EntityAgeable && ((EntityAgeable)event.getTarget()).getGrowingAge() < 0){
-				((EntityAgeable)event.getTarget()).addGrowth(24000); //This should work
+				System.out.println(":/");
+				System.out.println(((EntityAgeable)event.getTarget()).getGrowingAge());
+				((EntityAgeable)event.getTarget()).addGrowth(8000); //Starts at -24000, so takes 3 bone meal at most
 				if(!event.getEntityPlayer().capabilities.isCreativeMode)
 					event.getItemStack().stackSize--;
+				ItemDye.spawnBonemealParticles(event.getWorld(), event.getPos(), 15);
 			}
 		}
 	}
