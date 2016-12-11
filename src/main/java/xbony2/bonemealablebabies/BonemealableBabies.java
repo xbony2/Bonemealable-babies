6420d9abd8d00b8e91a00857260f16242ff7edeb
Eric Schneider <xbony22@gmail.com>
2016-12-11 18:48:27 -0500

    Ported to 1.11

--- src/main/java/xbony2/bonemealablebabies/BonemealableBabies.java
+++ src/main/java/xbony2/bonemealablebabies/BonemealableBabies.java
@@ -14,8 +14,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = BonemealableBabies.MODID, version = BonemealableBabies.VERSION)
 public class BonemealableBabies {
-	public static final String MODID = "bonemealablebabies";
-	public static final String VERSION = "1.0.0a";
+	public static final String MODID = "bonemealable_babies";
+	public static final String VERSION = "1.1.0a";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event){
@@ -28,7 +28,7 @@ public class BonemealableBabies {
 			if(!event.getWorld().isRemote && event.getItemStack() != null && event.getItemStack().getItem() == Items.DYE && event.getItemStack().getItemDamage() == 15 && event.getTarget() != null && event.getTarget() instanceof EntityAgeable && ((EntityAgeable)event.getTarget()).getGrowingAge() < 0){
 				((EntityAgeable)event.getTarget()).addGrowth(8000 / 20); //Starts at -24000, so takes 3 bone meal at most (also parameter is de-applified, read doc)
 				if(!event.getEntityPlayer().capabilities.isCreativeMode)
-					event.getItemStack().stackSize--;
+					event.getItemStack().func_190918_g(1); //func_190918_g(int p_190918_1_) ~= decreaseStackSize(int amount)
 				
 				event.getWorld().playEvent(2005, event.getPos(), 0); //ask minecraft, not me
 			}
