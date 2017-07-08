4d44f5a315d76bb2ca7c982703ab77c9f3fe7cf7
Eric Schneider <xbony22@gmail.com>
2017-07-07 21:42:42 -0400

    fix some stuff (maybe)

--- src/main/groovy/xbony2/bonemealablebabies/BonemealableBabies.groovy
+++ src/main/groovy/xbony2/bonemealablebabies/BonemealableBabies.groovy
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = BonemealableBabies.MODID, version = BonemealableBabies.VERSION)
+@Mod(modid = BonemealableBabies.MODID, version = BonemealableBabies.VERSION, modLanguageAdapter = "xbony2.forgegroove.GroovyAdapter")
 class BonemealableBabies {
 	public static final String MODID = "bonemealable_babies"
 	public static final String VERSION = "1.3.0a"
